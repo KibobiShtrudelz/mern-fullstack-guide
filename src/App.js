a0bbd8f1b3ef04a22d9d8528f70b3d63ea59e4b1
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2020-02-10 16:37:55 +0200

    69. Adding an "Auth" Page & Login Form - finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
-import UpdatePlace from './places/pages/UpdatePlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Auth from "./user/pages/Auth";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
@@ -18,18 +19,11 @@ const App = () => {
       <MainNavigation />
       <main>
         <Switch>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/places/:placeId">
-            <UpdatePlace />
-          </Route>
+          <Route path="/" exact component={Users} />
+          <Route path="/:userId/places" exact component={UserPlaces} />
+          <Route path="/places/new" exact component={NewPlace} />
+          <Route path="/places/:placeId" component={UpdatePlace} />
+          <Route path="/auth" component={Auth} />
           <Redirect to="/" />
         </Switch>
       </main>
